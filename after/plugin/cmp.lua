f2814c1fc7c89a555f977929861a75c42c7d2038
Sonny <ed22699@bristol.ac.uk>
2024-05-04 01:19:57 +0100

    Add the keybindings for autocomplete

---
+++ after/plugin/cmp.lua
@@ -0,0 +1,38 @@
+local cmp = require('cmp')
+
+cmp.setup({
+  sources = {
+    {name = 'nvim_lsp'},
+    {name = 'nvim_lua'},
+    {name = 'buffer'},
+  },
+  preselect = 'item',
+  completion = {
+      completeopt = 'menu,menuone,noinsert'
+  },
+  mapping = {
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<S-Tab>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
+    ['<Tab>'] = cmp.mapping.select_next_item({behavior = 'select'}),
+    ['<C-p>'] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.select_prev_item({behavior = 'insert'})
+      else
+        cmp.complete()
+      end
+    end),
+    ['<C-n>'] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.select_next_item({behavior = 'insert'})
+      else
+        cmp.complete()
+      end
+    end),
+  },
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+})
