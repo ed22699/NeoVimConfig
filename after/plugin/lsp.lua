a1f8f3904a99dcd2a022b6eee35067e9634665cd
Sonny <ed22699@bristol.ac.uk>
2024-05-04 01:08:47 +0100

    Create the text editor for C and lua

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,5 +1,30 @@
-local lsp = require('lsp-zero')
+local lsp_zero = require('lsp-zero')
 
-lsp.preset('recommended')
-lsp.setup()
--- TODO make this work
+lsp_zero.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp_zero.default_keymaps({
+      buffer = bufnr,
+      preserve_mappings = false
+  })
+end)
+
+-- here you can setup the language servers
+-- to learn how to use mason.nvim
+-- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guide/integrate-with-mason-nvim.md
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {},
+  handlers = {
+    function(server_name)
+      require('lspconfig').lua_ls.setup({})
+      require('lspconfig').csharp_ls.setup({})
+      require('lspconfig').typos_lsp.setup({})
+    end,
+  },
+})
+
+-- turn off diagnostics in gutter
+vim.diagnostic.config({
+  signs = false
+})
