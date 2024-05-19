5556b7f3e7100c8b50fc3a6601ee3b422655c619
Sonny Cooper <ed22699@bristol.ac.uk>
2024-05-19 22:11:33 +0100

    Think basic snippets are now working
    however custom ones are not

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -1,4 +1,8 @@
 local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+local cmp_format = require('lsp-zero').cmp_format({details = true})
+
+require('luasnip.loaders.from_vscode').lazy_load()
 
 cmp.setup({
   sources = {
@@ -16,6 +20,8 @@ cmp.setup({
     ['<C-e>'] = cmp.mapping.abort(),
     ['<S-Tab>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
     ['<Tab>'] = cmp.mapping.select_next_item({behavior = 'select'}),
+    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
     ['<C-p>'] = cmp.mapping(function()
       if cmp.visible() then
         cmp.select_prev_item({behavior = 'insert'})
