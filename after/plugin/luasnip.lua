dfb935f05a7d4cbfbbc639a50d8f17e8ff6357fc
Sonny Cooper <ed22699@bristol.ac.uk>
2024-05-20 23:12:14 +0100

    Learning how to write luasnip snippets

--- after/plugin/luasnip.lua
+++ after/plugin/luasnip.lua
@@ -2,11 +2,32 @@ local ls = require("luasnip")
 local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
+local extras = require("luasnip.extras")
+local rep = extras.rep
 
+vim.keymap.set({ "i", "s"}, "<A-k>", function()
+    if ls.expand_or_jumpable() then
+        ls.expand_or_jump()
+    end
+end, {silent = true})
+
+vim.keymap.set({ "i", "s"}, "<A-j>", function()
+    if ls.jumpable(-1) then
+        ls.jump(-1)
+    end
+end, {silent = true})
 ls.add_snippets("lua", {
     s("hello", {
         t('print("hello '),
         i(1),
         t(' world")')
+    })
+})
+
+ls.add_snippets("tex", {
+    s("beg", {
+        t("\\begin{"), i(1), t("}"),
+        t({ "", "\t"}), i(0),
+        t({ "","\\end{"}), rep(1), t("}"),
     })
 })
