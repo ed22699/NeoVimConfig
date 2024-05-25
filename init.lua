6d807e32e9ee6832f1f7c9bebca2135d6cc54fbf
Sonny Cooper <ed22699@bristol.ac.uk>
2024-05-25 20:14:18 +0100

    Keep focus on terminal when using vimtex view

--- init.lua
+++ init.lua
@@ -2,14 +2,9 @@ require("primary")
 vim.g.vimtex_compiler_progname = 'nvr'
 vim.g.vimtex_view_method = 'skim'
 
-vim.g.vimtex_view_skim_activate=1
+vim.g.vimtex_view_skim_activate=0
 
 vim.g.vimtex_view_skim_sync=1
 
 vim.g.vimtex_view_skim_reading_bar=1
 
-vim.cmd([[
-
-nmap <localleader>v <plug>(vimtex-view)
-
-]])
