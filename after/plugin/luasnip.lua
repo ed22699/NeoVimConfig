85598d3855a7d70a140c1520ac827db978696c19
Sonny Cooper <ed22699@bristol.ac.uk>
2024-06-25 19:33:41 +0100

    Add to begin

--- after/plugin/luasnip.lua
+++ after/plugin/luasnip.lua
@@ -50,6 +50,7 @@ ls.add_snippets("html", {
 <html lang="en">
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>{}</title>
     </head>
     <body>
